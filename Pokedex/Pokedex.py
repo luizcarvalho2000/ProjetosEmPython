9ac5cd0972ff03568da1877b0d4f7737f3cbb4db
Luiz  Carvalho <38143277+luizcarvalho2000@users.noreply.github.com>
2024-03-03 22:34:16 -0300

    Meu Primeiro Commit

--- Pokedex/Pokedex.py
+++ Pokedex/Pokedex.py
@@ -1,8 +1,8 @@
 from tkinter import *
 from tkinter import ttk
+from dados import *
 
 # Importando Pillow
-
 from PIL import Image, ImageTk
 
 # Cores
@@ -18,46 +18,77 @@ co6 = "#f7ff08"  #amarelo
 # Criando janelas
 
 janela = Tk()
-janela.title('')
-janela.geometry('550x510')
+janela.title('Pokedex')
+janela.geometry('905x510')
 janela.configure(bg=co1)
+janela.iconbitmap('images/pokeball.ico')
 
-ttk.Separator(janela, orient=HORIZONTAL).grid(row=0, columnspan=1, ipadx=272)
+ttk.Separator(janela, orient=HORIZONTAL).place(x=10, y=305, relwidth=1)
 
 estilo = ttk.Style(janela)
 estilo.theme_use("clam")
 
 # Criando frame Pokemon
-framePokemon = Frame(janela, width=550, height=290, relief='flat')
-framePokemon.grid(row=1, column=0)
-# framePokemon.configure(bg=co0)
+framePokemon = Frame(janela, width=885, height=304, relief='flat')
+framePokemon.pack(fill='both', expand=True)
+
+frameStatus = Frame(janela, width=350, height=204, relief='flat')
+frameStatus.place(x=10, y=300)
+
+def trocarPokemon(i):
+    global imagemPokemon, pokeImagem
+
+    # Trocando cor do frame
+    framePokemon['bg'] = pokemon[i]['tipo'][3]
+
+    # tipo de pokemon
+    pokeNome['text'] = i
+    pokeNome['bg'] = pokemon[i]['tipo'][3]
+    pokeTipo['text'] = pokemon[i]['tipo'][1]
+    pokeTipo['bg'] = pokemon[i]['tipo'][3]
+    pokeId['text'] = pokemon[i]['tipo'][0]
+    pokeId['bg'] = pokemon[i]['tipo'][3]
+
+    imagemPokemon = pokemon[i]['tipo'][2]
+
+    imagemPokemon = Image.open(imagemPokemon)
+    imagemPokemon = imagemPokemon.resize((238, 238))
+    imagemPokemon = ImageTk.PhotoImage(imagemPokemon)
+
+    pokeImagem = Label(framePokemon, image=imagemPokemon, relief='flat', bg=pokemon[i]['tipo'][3], fg=co1)
+    pokeImagem.place(x=50, y=75)
+
+    pokeTipo.lift()
+
+    # pokeStatus
+    pokeHp['text'] = pokemon[i]['status'][0]
+    pokeAtack['text'] = pokemon[i]['status'][1]
+    pokeDef['text'] = pokemon[i]['status'][2]
+    pokeVel['text'] = pokemon[i]['status'][3]
+    pokeTotal['text'] = pokemon[i]['status'][4]
+
+    # PokeHabilidade
+
+    pokeHab1['text'] = pokemon[i]['habilidades'][0]
+    pokeHab2['text'] = pokemon[i]['habilidades'][1]
+    pokeHab3['text'] = pokemon[i]['habilidades'][2]
+    pokeHab4['text'] = pokemon[i]['habilidades'][3]
+
 
 # Criando label
 
 # Nome
-pokeNome = Label(framePokemon, text='Pikachu', relief='flat', anchor=CENTER, font=('Fixedsys 20'), bg=co1, fg=co4)
+pokeNome = Label(framePokemon, text='', relief='flat', anchor=CENTER, font=('Fixedsys 20'), fg=co1)
 pokeNome.place(x=12, y=15)
-pokeNome.configure(bg=co6)
+
 
 # Categoria
-pokeTipo = Label(framePokemon, text='Eletrico', relief='flat', anchor=CENTER, font=('Ivy 10 bold'), bg=co1, fg=co0)
+pokeTipo = Label(framePokemon, text='', relief='flat', anchor=CENTER, font=('Ivy 10 bold'), fg=co1)
 pokeTipo.place(x=12, y=50)
 
 # Id
-pokeId = Label(framePokemon, text='#025', relief='flat', anchor=CENTER, font=('Ivy 10 bold'), bg=co1, fg=co0)
+pokeId = Label(framePokemon, text='', relief='flat', anchor=CENTER, font=('Ivy 10 bold'), fg=co1)
 pokeId.place(x=12, y=75)
-
-# Imagem do pokemon
-
-imagemPokemon = Image.open("C:\Projetos pessoais\Python\Pokedex\images\pikachu.png")
-imagemPokemon = imagemPokemon.resize((238, 238))
-imagemPokemon = ImageTk.PhotoImage(imagemPokemon)
-
-pokeImagem = Label(framePokemon, image=imagemPokemon, relief='flat', bg=co1, fg=co0)
-pokeImagem.place(x=12, y=75)
-
-pokeTipo.lift()
-pokeId.lift()
 
 # Status
 
@@ -66,23 +97,23 @@ pokeStatus.place(x=15, y=310)
 
 # Hp
 
-pokeHp = Label(janela, text='HP: 300', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+pokeHp = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pokeHp.place(x=15, y=360)
 
 # Ataque
-pokeAtack = Label(janela, text='Ataque: 600', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+pokeAtack = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pokeAtack.place(x=15, y=385)
 
 # Defesa
-pokeDef = Label(janela, text='Defesa: 500', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+pokeDef = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pokeDef.place(x=15, y=410)
 
 # Velocidade
-pokeVel = Label(janela, text='Velocidade: 200', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+pokeVel = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pokeVel.place(x=15, y=435)
 
 # Total
-pokeTotal = Label(janela, text='Total: 1600', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
+pokeTotal = Label(janela, text='', relief='flat', anchor=CENTER, font=('Verdana 10'), bg=co1, fg=co4)
 pokeTotal.place(x=15, y=460)
 
 # Habilidades
@@ -111,11 +142,223 @@ pokeHab4.place(x=195, y=435)
 
 # Imagem do pokemon
 
-imagemPokemon1 = Image.open("C:\Projetos pessoais\Python\Pokedex\images\cabeca-pikachu.png")
+imagemPokemon1 = Image.open("images/cabeça-bulbasaur.png")
 imagemPokemon1 = imagemPokemon1.resize((40, 40))
 imagemPokemon1 = ImageTk.PhotoImage(imagemPokemon1)
 
-btnPokeImagem1 = Button(framePokemon, image=imagemPokemon1, relief='flat', overrelief=RIDGE, compound=LEFT, anchor=NW, padx=5, font=('Verdana 12'), bg=co1, fg=co0)
-btnPokeImagem1.place(x=260, y=75)
+btnPokeImagem1 = Button(janela, image=imagemPokemon1, text="Bulbasaur",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Bulbasaur"))
+btnPokeImagem1.place(x=375, y=5)
+
+imagemPokemon2 = Image.open("images/cabeça-Ivysaur.png")
+imagemPokemon2 = imagemPokemon2.resize((40, 40))
+imagemPokemon2 = ImageTk.PhotoImage(imagemPokemon2)
+
+btnPokeImagem2 = Button(janela, image=imagemPokemon2, text="Ivysaur",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Ivysaur"))
+btnPokeImagem2.place(x=375, y=60)
+
+imagemPokemon3 = Image.open("images/cabeça-Venusaur.png")
+imagemPokemon3 = imagemPokemon3.resize((40, 40))
+imagemPokemon3 = ImageTk.PhotoImage(imagemPokemon3)
+
+btnPokeImagem3 = Button(janela, image=imagemPokemon3, text="Venusaur",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Venusaur"))
+btnPokeImagem3.place(x=375, y=115)
+
+imagemPokemon4 = Image.open("images/cabeça-charmander.png")
+imagemPokemon4 = imagemPokemon4.resize((40, 40))
+imagemPokemon4 = ImageTk.PhotoImage(imagemPokemon4)
+
+btnPokeImagem4 = Button(janela, image=imagemPokemon4, text="Charmander",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Charmander"))
+btnPokeImagem4.place(x=375, y=170)
+
+imagemPokemon5 = Image.open("images/cabeça-Charmeleon.png")
+imagemPokemon5 = imagemPokemon5.resize((40, 40))
+imagemPokemon5 = ImageTk.PhotoImage(imagemPokemon5)
+
+btnPokeImagem5 = Button(janela, image=imagemPokemon5, text="Charmeleon",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Charmeleon"))
+btnPokeImagem5.place(x=375, y=225)
+
+imagemPokemon6 = Image.open("images/cabeça-Charizard.png")
+imagemPokemon6 = imagemPokemon6.resize((40, 40))
+imagemPokemon6 = ImageTk.PhotoImage(imagemPokemon6)
+
+btnPokeImagem6 = Button(janela, image=imagemPokemon6, text="Charizard",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Charizard"))
+btnPokeImagem6.place(x=375, y=280)
+
+imagemPokemon7 = Image.open("images/cabeça-Squirtle.png")
+imagemPokemon7 = imagemPokemon7.resize((40, 40))
+imagemPokemon7 = ImageTk.PhotoImage(imagemPokemon7)
+
+btnPokeImagem7 = Button(janela, image=imagemPokemon7, text="Squirtle",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Squirtle"))
+btnPokeImagem7.place(x=375, y=335)
+
+imagemPokemon8 = Image.open("images/cabeça-Wartortle.png")
+imagemPokemon8 = imagemPokemon8.resize((40, 40))
+imagemPokemon8 = ImageTk.PhotoImage(imagemPokemon8)
+
+btnPokeImagem8 = Button(janela, image=imagemPokemon8, text="Wartortle",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Wartortle"))
+btnPokeImagem8.place(x=375, y=390)
+
+imagemPokemon9 = Image.open("images/cabeça-Blastoise.png")
+imagemPokemon9 = imagemPokemon9.resize((40, 40))
+imagemPokemon9 = ImageTk.PhotoImage(imagemPokemon9)
+
+btnPokeImagem9 = Button(janela, image=imagemPokemon9, text="Blastoise",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Blastoise"))
+btnPokeImagem9.place(x=375, y=445)
+
+imagemPokemon10 = Image.open("images/cabeça-Carterpie.png")
+imagemPokemon10 = imagemPokemon10.resize((40, 40))
+imagemPokemon10 = ImageTk.PhotoImage(imagemPokemon10)
+
+btnPokeImagem10 = Button(janela, image=imagemPokemon10, text="Caterpie",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Caterpie"))
+btnPokeImagem10.place(x=550, y=5)
+
+imagemPokemon11 = Image.open("images/cabeça-Metapod.png")
+imagemPokemon11 = imagemPokemon11.resize((40, 40))
+imagemPokemon11 = ImageTk.PhotoImage(imagemPokemon11)
+
+btnPokeImagem11 = Button(janela, image=imagemPokemon11, text="Metapod",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Metapod"))
+btnPokeImagem11.place(x=550, y=60)
+
+imagemPokemon12 = Image.open("images/cabeça-Butterfree.png")
+imagemPokemon12 = imagemPokemon12.resize((40, 40))
+imagemPokemon12 = ImageTk.PhotoImage(imagemPokemon12)
+
+btnPokeImagem12 = Button(janela, image=imagemPokemon12, text="Butterfree",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Butterfree"))
+btnPokeImagem12.place(x=550, y=115)
+
+imagemPokemon13 = Image.open("images/cabeça-Pichu.png")
+imagemPokemon13 = imagemPokemon13.resize((40, 40))
+imagemPokemon13 = ImageTk.PhotoImage(imagemPokemon13)
+
+btnPokeImagem13 = Button(janela, image=imagemPokemon13, text="Pichu",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("pichu"))
+btnPokeImagem13.place(x=550, y=170)
+
+imagemPokemon14 = Image.open("images/cabeça-pikachu.png")
+imagemPokemon14 = imagemPokemon14.resize((40, 40))
+imagemPokemon14 = ImageTk.PhotoImage(imagemPokemon14)
+
+btnPokeImagem14 = Button(janela, image=imagemPokemon14, text="Pikachu",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Pikachu"))
+btnPokeImagem14.place(x=550, y=225)
+
+imagemPokemon15 = Image.open("images/cabeça-Raichu.png")
+imagemPokemon15 = imagemPokemon15.resize((40, 40))
+imagemPokemon15 = ImageTk.PhotoImage(imagemPokemon15)
+
+btnPokeImagem15 = Button(janela, image=imagemPokemon15, text="Raichu",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Raichu"))
+btnPokeImagem15.place(x=550, y=280)
+
+imagemPokemon16 = Image.open("images/cabeça-Magikarp.png")
+imagemPokemon16 = imagemPokemon16.resize((40, 40))
+imagemPokemon16 = ImageTk.PhotoImage(imagemPokemon16)
+
+btnPokeImagem16 = Button(janela, image=imagemPokemon16, text="Magikarp",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Magikarp"))
+btnPokeImagem16.place(x=550, y=335)
+
+imagemPokemon17 = Image.open("images/cabeça-gyarados.png")
+imagemPokemon17 = imagemPokemon17.resize((40, 40))
+imagemPokemon17 = ImageTk.PhotoImage(imagemPokemon17)
+
+btnPokeImagem17 = Button(janela, image=imagemPokemon17, text="Gyarados",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Gyarados"))
+btnPokeImagem17.place(x=550, y=390)
+
+imagemPokemon18 = Image.open("images/cabeça-Gastly.png")
+imagemPokemon18 = imagemPokemon18.resize((40, 40))
+imagemPokemon18 = ImageTk.PhotoImage(imagemPokemon18)
+
+btnPokeImagem18 = Button(janela, image=imagemPokemon18, text="Gastly",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Gastly"))
+btnPokeImagem18.place(x=550, y=445)
+
+imagemPokemon19 = Image.open("images/cabeça-Haunter.png")
+imagemPokemon19 = imagemPokemon19.resize((40, 40))
+imagemPokemon19 = ImageTk.PhotoImage(imagemPokemon19)
+
+btnPokeImagem19 = Button(janela, image=imagemPokemon19, text="Haunter",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Haunter"))
+btnPokeImagem19.place(x=725, y=5)
+
+imagemPokemon20 = Image.open("images/cabeça-gengar.png")
+imagemPokemon20 = imagemPokemon20.resize((40, 40))
+imagemPokemon20 = ImageTk.PhotoImage(imagemPokemon20)
+
+btnPokeImagem20 = Button(janela, image=imagemPokemon20, text="Gengar",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Gengar"))
+btnPokeImagem20.place(x=725, y=60)
+
+imagemPokemon21 = Image.open("images/cabeça-Snorlax.png")
+imagemPokemon21 = imagemPokemon21.resize((40, 40))
+imagemPokemon21 = ImageTk.PhotoImage(imagemPokemon21)
+
+btnPokeImagem21 = Button(janela, image=imagemPokemon21, text="Snorlax",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Snorlax"))
+btnPokeImagem21.place(x=725, y=115)
+
+imagemPokemon22 = Image.open("images/cabeça-Articuno.png")
+imagemPokemon22 = imagemPokemon22.resize((40, 40))
+imagemPokemon22 = ImageTk.PhotoImage(imagemPokemon22)
+
+btnPokeImagem22 = Button(janela, image=imagemPokemon22, text="Articuno",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Articuno"))
+btnPokeImagem22.place(x=725, y=170)
+
+imagemPokemon23 = Image.open("images/cabeça-Zapdos.png")
+imagemPokemon23 = imagemPokemon23.resize((40, 40))
+imagemPokemon23 = ImageTk.PhotoImage(imagemPokemon23)
+
+btnPokeImagem23 = Button(janela, image=imagemPokemon23, text="Zapdos",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Zapdos"))
+btnPokeImagem23.place(x=725, y=225)
+
+imagemPokemon24 = Image.open("images/cabeça-Moltres.png")
+imagemPokemon24 = imagemPokemon24.resize((40, 40))
+imagemPokemon24 = ImageTk.PhotoImage(imagemPokemon24)
+
+btnPokeImagem24 = Button(janela, image=imagemPokemon24, text="Moltres",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Moltres"))
+btnPokeImagem24.place(x=725, y=280)
+
+imagemPokemon25 = Image.open("images/cabeça-Dragonite.png")
+imagemPokemon25 = imagemPokemon25.resize((40, 40))
+imagemPokemon25 = ImageTk.PhotoImage(imagemPokemon25)
+
+btnPokeImagem25 = Button(janela, image=imagemPokemon25, text="Dragonite",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Dragonite"))
+btnPokeImagem25.place(x=725, y=335)
+
+imagemPokemon26 = Image.open("images/cabeça-Mewtwo.png")
+imagemPokemon26 = imagemPokemon26.resize((40, 40))
+imagemPokemon26 = ImageTk.PhotoImage(imagemPokemon26)
+
+btnPokeImagem26 = Button(janela, image=imagemPokemon26, text="Mewtwo",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Mewtwo"))
+btnPokeImagem26.place(x=725, y=390)
+
+imagemPokemon27 = Image.open("images/cabeça-Mew.png")
+imagemPokemon27 = imagemPokemon27.resize((40, 40))
+imagemPokemon27 = ImageTk.PhotoImage(imagemPokemon27)
+
+btnPokeImagem27 = Button(janela, image=imagemPokemon27, text="Mew",width=150, relief=RAISED, overrelief=RIDGE,
+                        compound=LEFT, anchor=NW,  padx=5, font=('Verdana 12 bold'), bg=co1, fg=co0, command=lambda:trocarPokemon("Mew"))
+btnPokeImagem27.place(x=725, y=445)
+
+# Inicializando com o primeiro pokemon
+trocarPokemon("Bulbasaur")
 
 janela.mainloop()
---
+++ Pokedex/dados.py
@@ -0,0 +1,139 @@
+
+pokemon = {
+    "Bulbasaur": {
+        "status": ['HP: 320','Ataque: 600', 'Defesa: 500', 'Velocidade: 200', 'Total: 1620'],
+        "habilidades": ['Chicote de cipó', 'Bomba de Semente', 'Folha navalha', 'Semente sangue-suga'],
+        "tipo": ['#01', 'Grama', 'images/bulbasaur.png', "#49D0B0"],
+    },
+    "Ivysaur": {
+        "status": ['HP: 350', 'Ataque: 650', 'Defesa: 550', 'Velocidade: 250', 'Total: 1800'],
+        "habilidades": ['Chicote Toxico', 'Chicote de Vinha', 'Folha navalha', 'Aroma Convocador'],
+        "tipo": ['#02', 'Grama', 'images/Ivysaur.png', "#49D0B0"],
+    },
+    "Venusaur": {
+        "status": ['HP: 400', 'Ataque: 700', 'Defesa: 600', 'Velocidade: 300', 'Total: 2000'],
+        "habilidades": ['Chicote de cipó', 'Raio solar', 'Florecer GMax', 'Tufão Solar'],
+        "tipo": ['#03', 'Grama', 'images/Venusaur.png', "#49D0B0"],
+    },
+    "Charmander": {
+        "status": ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 200', 'Total: 1600'],
+        "habilidades": ['Calda em Chamas', 'Arranhão', 'Lança-chamas', 'Presa de Fogo'],
+        "tipo": ['#04', 'Fogo', 'images/charmander.png', '#ED8A8B'],
+    },
+    "Charmeleon": {
+        "status": ['HP: 350', 'Ataque: 650', 'Defesa: 550', 'Velocidade: 250', 'Total: 1800'],
+        "habilidades": ['Raio de Calor', 'Garra de metal', 'Lança-chamas', 'Fúria'],
+        "tipo": ['#05', 'Fogo', 'images/Charmeleon.png', '#ED8A8B'],
+    },
+    "Charizard": {
+        "status": ['HP: 400', 'Ataque: 700', 'Defesa: 600', 'Velocidade: 300', 'Total: 2000'],
+        "habilidades": ['Chamas Furacão', 'Garra de metal', 'Lança-chamas', 'Tempestade Carmim'],
+        "tipo": ['#06', 'Fogo', 'images/Charizard.png', '#ED8A8B'],
+    },
+    "Squirtle": {
+        "status": ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 200', 'Total: 1600'],
+        "habilidades": ['Jato de água', 'Chuva Borrifante', 'Bolhas', 'Investida de água'],
+        "tipo": ['#07', 'Água', 'images/Squirtle.png', "#76BEFE"],
+    },
+    "Wartortle": {
+        "status": ['HP: 350', 'Ataque: 650', 'Defesa: 550', 'Velocidade: 250', 'Total: 1800'],
+        "habilidades": ['Jato de água', 'Revolver de Agua', 'Cachoeira', 'Investida de água'],
+        "tipo": ['#08', 'Água', 'images/Wartortle.png', "#76BEFE"],
+    },
+    "Blastoise": {
+        "status": ['HP: 400', 'Ataque: 700', 'Defesa: 600', 'Velocidade: 300', 'Total: 2000'],
+        "habilidades": ['Lançador de Bolhas', 'Borrifada de Foguete', 'Hidro bomba', 'Tormenta Poderosa'],
+        "tipo": ['#09', 'Água', 'images/Blastoise.png', "#76BEFE"],
+    },
+    "Caterpie": {
+        "status": ['HP: 200', 'Ataque: 400', 'Defesa: 300', 'Velocidade: 100', 'Total: 1000'],
+        "habilidades": ['Investida', 'Evolução Adaptativa', 'Ataque Surpresa', 'Crisálida'],
+        "tipo": ['#10', 'Inseto', 'images/Caterpie.png', "#A8B820"],
+    },
+    "Metapod": {
+        "status": ['HP: 250', 'Ataque: 450', 'Defesa: 350', 'Velocidade: 150', 'Total: 1200'],
+        "habilidades": ['Defesa de Ferro', 'Picada', 'Exoesqueleto', 'Emergir'],
+        "tipo": ['#11', 'Inseto', 'images/Metapod.png', "#A8B820"],
+    },
+    "Butterfree": {
+        "status": ['HP: 300', 'Ataque: 500', 'Defesa: 400', 'Velocidade: 200', 'Total: 1400'],
+        "habilidades": ['ataque de Vento', 'Raio psíquico', 'Lufada de Vento', 'Raio Solar'],
+        "tipo": ['#12', 'Inseto', 'images/Butterfree.png', "#A8B820"],
+    },
+    "pichu": {
+        "status": ['HP: 250', 'Ataque: 200', 'Defesa: 100', 'Velocidade: 100', 'Total: 650'],
+        "habilidades": ['Choque do trovão', 'Ataque rápido', 'Bola elétrica', 'Choque do trovão'],
+        "tipo": ['#172', 'Elétrico', 'images/pichu.png', "#FCC719"],
+    },
+    "Pikachu": {
+        "status": ['HP: 300', 'Ataque: 600', 'Defesa: 500', 'Velocidade: 200', 'Total: 1600'],
+        "habilidades": ['Choque do trovão', 'Calda de ferro', 'Bola elétrica', 'Ataque rápido'],
+        "tipo": ["#25", "Elétrico", "images/pikachu.png", "#FCC719"],
+
+    },
+    "Raichu": {
+        "status": ['HP: 350', 'Ataque: 650', 'Defesa: 550', 'Velocidade: 250', 'Total: 1800'],
+        "habilidades": ['Choque do trovão', 'Relâmpago', 'Surfista Eletrico', 'Ataque rápido'],
+        "tipo": ['#26', 'Elétrico', 'images/Raichu.png', "#FCC719"],
+    },
+    "Magikarp": {
+        "status": ['HP: 100', 'Ataque: 300', 'Defesa: 200', 'Velocidade: 50', 'Total: 650'],
+        "habilidades": ['Super Borrifada', 'Pula Fora', 'Escamas Douradas', 'Operação Dragão'],
+        "tipo": ['#129', 'Água', 'images/Magikarp.png', "#76BEFE"],
+    },
+    "Gyarados": {
+        "status": ['HP: 600', 'Ataque: 800', 'Defesa: 700', 'Velocidade: 300', 'Total: 2400'],
+        "habilidades": ['Hidro bomba', 'Hiper Raio', 'Ira do Dragão', 'Calda Tirânica'],
+        "tipo": ['#130', 'Água', 'images/gyarados.png', "#76BEFE"],
+    },
+    "Gastly": {
+        "status": ['HP: 200', 'Ataque: 400', 'Defesa: 300', 'Velocidade: 100', 'Total: 1000'],
+        "habilidades": ['Desaparecer', 'Olhos Tenebrosos', 'Nevoeiro de Fumaça', 'Pulso Sonolento'],
+        "tipo": ['#92', 'Fantasma', 'images/gastly.png', "#BA68C8"],
+    },
+    "Haunter": {
+        "status": ['HP: 300', 'Ataque: 500', 'Defesa: 400', 'Velocidade: 200', 'Total: 1400'],
+        "habilidades": ['Lambida', 'Pesadelo', 'Névoa Sinistra', 'Ampliador de Dor'],
+        "tipo": ['#93', 'Fantasma', 'images/haunter.png', "#BA68C8"],
+    },
+    "Gengar": {
+        "status": ['HP: 400', 'Ataque: 700', 'Defesa: 600', 'Velocidade: 250', 'Total: 2300'],
+        "habilidades": ['Soco sombrio', 'Bola sombria', 'Veneno do Crepúsculo', 'Borrifada Hipinótica'],
+        "tipo": ['#94', 'Fantasma', 'images/gengar.png', "#BA68C8"],
+    },
+    "Snorlax": {
+        "status": ['HP: 800', 'Ataque: 900', 'Defesa: 800', 'Velocidade: 350', 'Total: 2850'],
+        "habilidades": ['Impacto Pesado', 'Hiper Raio', 'Pancada', 'Gigante'],
+        "tipo": ['#143', 'Normal', 'images/snorlax.png', "#A8A878"],
+    },
+    "Articuno": {
+        "status": ['HP: 700', 'Ataque: 900', 'Defesa: 800', 'Velocidade: 350', 'Total: 2850'],
+        "habilidades": ['Vento Gelado', 'Vendaval', 'Gelo Cortante', 'Gelo de Aurora'],
+        "tipo": ['#144', 'Gelo', 'images/articuno.png', "#98D8D8"],
+    },
+    "Zapdos": {
+        "status": ['HP: 700', 'Ataque: 900', 'Defesa: 800', 'Velocidade: 350', 'Total: 2850'],
+        "habilidades": ['Choque do trovão', 'Relâmpago', 'Ataque de Vento', 'Tempestade Elétrica'],
+        "tipo": ['#145', 'Elétrico', 'images/Zapdos.png', "#FCC719"],
+    },
+    "Moltres": {
+        "status": ['HP: 700', 'Ataque: 900', 'Defesa: 800', 'Velocidade: 350', 'Total: 2850'],
+        "habilidades": ['Chamas Furacão', 'Garra de metal', 'Lança-chamas', 'Tempestade Carmim'],
+        "tipo": ['#146', 'Fogo', 'images/moltres.png', "#ED8A8B"],
+    },
+    "Dragonite": {
+        "status": ['HP: 700', 'Ataque: 900', 'Defesa: 800', 'Velocidade: 350', 'Total: 2800'],
+        "habilidades": ['Soco do dragão', 'Vendaval Do Dragão', 'Hiper Raio', 'Calda Aniquiladora'],
+        "tipo": ['#149', 'Dragão', 'images/dragonite.png', "#C29791"],
+    },
+    "Mewtwo": {
+        "status": ['HP: 700', 'Ataque: 900', 'Defesa: 800', 'Velocidade: 350', 'Total: 2850'],
+        "habilidades": ['Confusão', 'Super Raio Psíquico', 'Bola Psíquica', 'Telicinese'],
+        "tipo": ['#150', 'Psíquico', 'images/mewtwo.png', "#F85888"],
+    },
+    "Mew": {
+        "status": ['HP: 700', 'Ataque: 900', 'Defesa: 800', 'Velocidade: 350', 'Total: 2850'],
+        "habilidades": ['Confusão', 'Raio Psíquico', 'Hackear Genoma', 'Perfeição'],
+        "tipo": ['#151', 'Psíquico', 'images/mew.png', "#F85888"],
+    }
+}
